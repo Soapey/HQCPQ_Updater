587992443ba21b05c823c019e763ded06b9eac25
Grant Soper <granttravissoper@gmail.com>
2023-02-09 14:48:07 +1100

    -a

---
+++ updater/updater.py
@@ -0,0 +1,30 @@
+import urllib.request
+import configparser
+import os
+
+def resource_path(relative_path):
+
+    base_path = os.path.abspath(".")
+
+    full_path = os.path.join(base_path, relative_path)
+
+    return full_path
+
+
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(resource_path(r"files\config.ini"))
+    return config
+
+
+def main():
+    
+    config = read_config()
+    
+    download_link = config['AppSettings']['update_file_url']
+    save_to_path = resource_path("HQCPQ.exe")
+
+    filename, headers = urllib.request.urlretrieve(download_link, save_to_path)
+
+if __name__ == "__main__":
+    main()
